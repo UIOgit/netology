e7eb41dd004042675faaf2ffe39875f6a6c65d57
Ivan <ivan7keys@mail.ru>
2023-05-23 16:33:50 +0300

    изменено отслеживание

--- m01/1.5/config.js
+++ m01/1.5/config.js
@@ -1,2 +1,1 @@
-apiKey = f55715e7ff17f32de5b922fa44fb05f9;
-apiURL = 'https://api.weatherstack.com/current';
+apiKey = f55715e7ff17f32de5b922fa44fb05f9
--- m01/1.5/task01.js
+++ m01/1.5/task01.js
@@ -1,6 +1,23 @@
 const donenv = require('dotenv');
 const http = require ('http');
+const city = 'London';
 
-http.get(process.env.apiURL,(result)=>{
+const testURL = 'http://api.weatherstack.com/current?access_key=f55715e7ff17f32de5b922fa44fb05f9&query=London';
+
+http.get(process.env.apiURL + '?access_key=' + process.env.apiKey + '&query='+ city, (result)=>{
     const {statusCode} = result;
+        if (statusCode !== 200){
+            console.log(`statusCode: ${statusCode}`)
+        return
+    }
+
+    result.setEncoding('utf8');
+    let rowData = '';
+    result.on('data', (chunk) => rowData += chunk);
+    result.on('end', () => {
+        let parseData = JSON.parse(rowData);
+        console.log(parseData);
+    })
+}).on('error', (error) => {
+    console.error(error)
 })
